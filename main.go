dfa52618f98aeb048415326379b269a699a8ab99
mahanth <22050509+gnmahanth@users.noreply.github.com>
2024-09-09 18:46:29 +0530

    handle service account creds for aws eks cluster (#23)

--- main.go
+++ main.go
@@ -90,8 +90,11 @@ func main() {
 
 	switch config.CloudProvider {
 	case util.CloudProviderAWS:
-		if config.AWSCredentialSource != "EcsContainer" && config.AWSCredentialSource != "Ec2InstanceMetadata" && config.AWSCredentialSource != "Environment" {
-			log.Fatal().Msgf("invalid AWS_CREDENTIAL_SOURCE - should be one of EcsContainer, Ec2InstanceMetadata, Environment")
+		if config.AWSCredentialSource != "EcsContainer" &&
+			config.AWSCredentialSource != "Ec2InstanceMetadata" &&
+			config.AWSCredentialSource != "Environment" &&
+			config.AWSCredentialSource != "ServiceAccount" {
+			log.Fatal().Msgf("invalid AWS_CREDENTIAL_SOURCE - should be one of EcsContainer, Ec2InstanceMetadata, Environment, ServiceAccount")
 		}
 		if config.IsOrganizationDeployment && config.RoleName == "" {
 			log.Fatal().Msgf("ROLE_NAME is required in aws installation")
